Fix error in ABC calculation
- Implement the idea of CM into the calculation of ABC
- Correct the formula for rotational constants

i_tensor.py
@@ -34,29 +34,58 @@ def i_single_pm(m, r_vec):
 
     return i_tens
 
-# I tensor for multiple point masses
-def i_mpm(m_list, rvec_list):
+def guard_list_len(list_a, list_b):
     try:
-        m_count = len(m_list)
-        r_count = len(rvec_list)
-        if m_count != r_count:
-            raise Exception("wrong lists")
+        if len(list_a) != len(list_b):
+            raise Exception("List lengths don't match.")
         else:
-            count = m_count
+            return len(list_a)
     except Exception as e:
         print(e)
 
+# Center of Mass for multiple point masses
+def cm_mpm(m_list, rvec_list):
+    count = guard_list_len(m_list, rvec_list)
+    lrange = range(0, count)
+
+    m_tot = sum(m_list)
+    t_vec = [0, 0, 0]
+
+    for n in lrange:
+        rvec = rvec_list[n]
+        m = m_list[n]
+        for i in INDEX_RANGE:
+            t_vec[i] = t_vec[i] + rvec[i] * m
+
+    cm_vec = [c / m_tot for c in t_vec]
+
+    return cm_vec
+
+
+# I tensor for multiple point masses
+def i_mpm(m_list, rvec_list, origin = None):
+    count = guard_list_len(m_list, rvec_list)
+    lrange = range(0, count)
     i_tens = i_init()
 
-    lrange = range(0, count)
+    if origin is None:
+        cm_vec = cm_mpm(m_list, rvec_list)
+        r_list = [np.array(rvec) - np.array(cm_vec) for rvec in rvec_list]
+    else:
+        try:
+            r_list = [np.array(rvec) - np.array(origin) for rvec in rvec_list]
+        except Exception as e:
+            print(e)
+        else:
+            pass
 
     #Calculate I tensor
     for i in INDEX_RANGE:
         for j in INDEX_RANGE:
             if i == j:
-                i_tens[i][j] = np.sum([m_list[n]*(np.sum([rvec_list[n][k]**2 for k in filter(lambda l: l!= i, INDEX_RANGE)])) for n in lrange])
+                i_tens[i][j] = np.sum([m_list[n]*(np.sum([r_list[n][k]**2 for k in filter(lambda l: l!= i, INDEX_RANGE)])) for n in lrange])
             else:
-                i_tens[i][j] = np.sum([m_list[n]*rvec_list[n][i]*rvec_list[n][j] for n in lrange])
+                i_tens[i][j] = np.sum([m_list[n]*rvec_list[n][i]*r_list[n][j] for n in lrange])
 
     return i_tens
 
@@ -65,9 +94,9 @@ def abc_cgs_from_i(i_tens, toMHz=True):
     abc = []
 
     for i in INDEX_RANGE:
-        b = H/(8*(PI**2)*C*i_tens[i][i])
+        b = H/(8*(PI**2)*i_tens[i][i])
         if toMHz:
-            b = b * C * (10**(-6))
+            b = b * (10**(-6))
         abc.append(b)
 
     abc.sort(reverse=True)